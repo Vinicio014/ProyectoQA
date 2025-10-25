02370c265fd049cc69e2e6eb4ef9fa4756ca0299
yoanan1996 <jvasquezl17@miumg.edu.gt>
2025-10-24 21:32:04 -0600

    Mis cambios nuevos

--- backend/conf/database.php
+++ backend/conf/database.php
@@ -5,7 +5,7 @@ if (!class_exists('Database')) {
         private $host = "localhost";
         private $db_name = "Uniformes_deportivos";
         private $username = "root";
-        private $password = "Lopez40.";
+        private $password = "esssecond";
         private $conn;
 
         public function getConnection() {
